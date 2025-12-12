242fee91c225b7fd801d3822310d06cbbb715656
Nico Aeberhard <nico.aeberhard@roche.com>
2025-12-12 10:36:04 +0100

    Aufgabe 6

---
+++ init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS demo;
+
+USE demo;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(50)
+);
+
+INSERT INTO users (name, email) VALUES
+('Alice', 'alice@example.com'),
+('Bob', 'bob@example.com'),
+('Charlie', 'charlie@example.com');
